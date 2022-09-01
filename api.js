10a3a7fc13ae75de6f4a982f304f0bb33bb59fcb
Bruno Carnavalli <carnavallibruno@gmail.com>
2022-09-01 03:49:15 -0300

    Added Previous and Next buttons so the user can navigate between characters.

--- api.js
+++ api.js
@@ -1,37 +1,105 @@
-async function getStarWarsAPI() {
-  console.log('chamou')
+// ? Variables
+var $ = document.querySelector.bind(document);
+
+var characterId = 0;
+
+var previousButton = $('#previousButton');
+var nextButton = $('#nextButton');
+
+
+var characterName = $('#inputName');
+var height = $('#inputHeight');
+var mass = $('#inputMass');
+var hair = $('#inputHair');
+var skin = $('#inputSkin');
+var eye = $('#inputEye');
+var birthYear = $('#inputBirthYear');
+var gender = $('#inputGender');
+// var homeworld = $('#inputHomeworld');
+
+
+
+// ? Function that gets the API url
+async function getStarWarsCharactersAPI() {
+  const url = `https://swapi.dev/api/people/${characterId}/`
   try {
-    const swAPI = await fetch('https://swapi.dev/api/people/4/');
-    const swAPIConverted = await swAPI.json();
-    if (!swAPI.ok) {
+    const swCharactersAPI = await fetch(url);
+    const swCharactersAPIConverted = await swCharactersAPI.json();
+
+    // const swHomeworldsAPI = await fetch(swCharactersAPIConverted.homeworld)
+    // const swHomeworldsAPIConverted = await swHomeworldsAPI.json();
+
+
+    if (!swCharactersAPI.ok) {
       throw new Error ('Error in getting API')
     }
 
-    var $ = document.querySelector.bind(document);
+    characterName.value = swCharactersAPIConverted.name;
+    height.value = swCharactersAPIConverted.height;
+    mass.value = swCharactersAPIConverted.mass;
+    hair.value = swCharactersAPIConverted.hair_color;
+    skin.value = swCharactersAPIConverted.skin_color;
+    eye.value = swCharactersAPIConverted.eye_color;
+    birthYear.value = swCharactersAPIConverted.birth_year;
+    gender.value = swCharactersAPIConverted.gender;
 
-    var name = $('#inputName');
-    var height = $('#inputHeight');
-    var mass = $('#inputMass');
-    var hair = $('#inputHair');
-    var skin = $('#inputSkin');
-    var eye = $('#inputEye');
-    var birthYear = $('#inputBirthYear');
-    var gender = $('#inputGender');
+    // homeworld.value = swHomeworldsAPIConverted.name;
 
-    name.value = swAPIConverted.name;
-    height.value = swAPIConverted.height;
-    mass.value = swAPIConverted.mass;
-    hair.value = swAPIConverted.hair_color;
-    skin.value = swAPIConverted.skin_color;
-    eye.value = swAPIConverted.eye_color;
-    birthYear.value = swAPIConverted.birth_year;
-    gender.value = swAPIConverted.gender;
-
-    return swAPIConverted;
+    return swCharactersAPIConverted;
   }
   catch(error) {
     console.log(error)
   }
 }
 
-getStarWarsAPI();
+
+
+// ? Previous button event listener
+previousButton.addEventListener('click', function() {
+  if (characterId < 0) {
+    characterId = 0;
+    clearForm();
+    return false;
+    
+  } else {
+    characterId--;
+    getStarWarsAPI();
+  }
+})
+
+
+
+// ? Next button event listener
+nextButton.addEventListener('click', function() {
+  if (characterId < 0 || characterId > 83) {
+    characterId = 0;
+    clearForm();
+    return false;
+  } else {
+    characterId++;
+    getStarWarsAPI();
+  }
+})
+
+
+
+// ? Function that clears form data
+function clearForm() {
+  characterName.value = '';
+  height.value = '';
+  mass.value = '';
+  hair.value = '';
+  skin.value = '';
+  eye.value = '';
+  birthYear.value = '';
+  gender.value = '';
+}
+
+getStarWarsCharactersAPI();
+
+
+/* 
+TODO: make a different page for the homeworlds (planets) and export their names to this file
+TODO: create more JS files to maintain easy management within the code
+
+*/
